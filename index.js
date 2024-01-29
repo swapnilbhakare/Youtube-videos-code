23f7786040189e6bdb7cc43a7939ab0ea4b8e5d5
Swapnil bhakare <swapnilbhakare7@gmail.com>
2024-01-29 18:29:20 +0530

    access webcam using javascript

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(navigator.userAgent);
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(function (stream) {
+    var video = document.getElementById('webcam');
+    video.srcObject = stream;
+  })
+  .catch(function (error) {
+    if (error.name === 'NotAllowedError') {
+      console.error('Permission denied by user.');
+    } else {
+      console.error('Error accessing webcam:', error);
+    }
+  });
